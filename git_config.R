6b058e295f4a2f04e83727238d5d90150830e874
STRINGR BELLE <STRINGRBELLE@GMAIL.COM>
2020-03-31 06:26:08 +0000

    customise theme

--- git_config.R
+++ git_config.R
@@ -10,6 +10,6 @@ git_setup <- function(email, name) {
 
 # Now call our new function...
 git_setup(
-  email = "YOUR EMAIL ADDRESS",
-  name = "YOUR NAME HERE"  
+  email = "STRINGRBELLE@GMAIL.COM",
+  name = "STRINGR BELLE"  
 )
